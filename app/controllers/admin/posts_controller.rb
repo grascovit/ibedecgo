794ea4a4302330544cc651a6bbc36f9e91b02e3e
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-06-12 22:18:46 -0300

    create the posts controller

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,55 @@
+class Admin::PostsController < AdminController
+  before_action :set_post, only: %i[show edit update destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  def show; end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit; end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to admin_post_url(@post), notice: 'Notícia criada com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      redirect_to admin_post_url(@post), notice: 'Notícia atualizada com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to admin_posts_url, notice: 'Notícia excluída com sucesso.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image, :user_id)
+  end
+end
