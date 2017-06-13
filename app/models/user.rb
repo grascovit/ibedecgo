959e72bd99c01ccc19d12c36285166a940e539eb
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-06-12 22:38:24 -0300

    create method to return user full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,8 @@ class User < ApplicationRecord
                     styles: { medium: '300x300>', thumb: '100x100>' },
                     default_url: '/assets/:style/missing-avatar.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
