cad832e2df8b6e24c06133ab59c841bfd7cca282
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-06-19 11:17:57 -0300

    rename judgement helper methods

--- app/helpers/admin/judgements_helper.rb
+++ app/helpers/admin/judgements_helper.rb
@@ -1,9 +1,9 @@
 module Admin::JudgementsHelper
-  def category_i18n(key)
+  def judgement_category_i18n(key)
     t("activerecord.attributes.judgement.categories.#{key}")
   end
 
-  def category_options
-    Judgement.categories.map { |key, _value| [category_i18n(key), key] }
+  def judgement_category_options
+    Judgement.categories.map { |key, _value| [judgement_category_i18n(key), key] }
   end
 end
--- spec/helpers/admin/judgements_helper_spec.rb
+++ spec/helpers/admin/judgements_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Admin::JudgementsHelper, type: :helper do
-  describe '#category_i18n' do
+  describe '#judgement_category_i18n' do
     it 'returns the i18n judgement category' do
       judgement = create(:judgement, category: :consumer)
 
-      expect(helper.category_i18n(judgement.category)).to eq t('activerecord.attributes.judgement.categories.consumer')
+      expect(helper.judgement_category_i18n(judgement.category)).to eq t('activerecord.attributes.judgement.categories.consumer')
     end
   end
 
-  describe '#category_options' do
+  describe '#judgement_category_options' do
     it 'returns the i18n judgement categories hash' do
       consumer = [t('activerecord.attributes.judgement.categories.consumer'), 'consumer']
       social_security = [t('activerecord.attributes.judgement.categories.social_security'), 'social_security']
@@ -17,7 +17,7 @@ RSpec.describe Admin::JudgementsHelper, type: :helper do
       sfh = [t('activerecord.attributes.judgement.categories.sfh'), 'sfh']
       sfi = [t('activerecord.attributes.judgement.categories.sfi'), 'sfi']
 
-      expect(helper.category_options).to eq [construction, sfh, sfi, social_security, consumer]
+      expect(helper.judgement_category_options).to eq [construction, sfh, sfi, social_security, consumer]
     end
   end
 end
