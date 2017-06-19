43efa8fd6f97470d387544d1be0eea87a4b8f693
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-06-19 10:25:38 -0300

    create methods to translate judgements categories

--- app/helpers/admin/judgements_helper.rb
+++ app/helpers/admin/judgements_helper.rb
@@ -1,2 +1,9 @@
 module Admin::JudgementsHelper
+  def category_i18n(key)
+    t("activerecord.attributes.judgement.categories.#{key}")
+  end
+
+  def category_options
+    Judgement.categories.map { |key, _value| [category_i18n(key), key] }
+  end
 end
--- spec/helpers/admin/judgements_helper_spec.rb
+++ spec/helpers/admin/judgements_helper_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Admin::JudgementsHelper, type: :helper do
+  describe '#category_i18n' do
+    it 'returns the i18n judgement category' do
+      judgement = create(:judgement, category: :consumer)
+
+      expect(helper.category_i18n(judgement.category)).to eq t('activerecord.attributes.judgement.categories.consumer')
+    end
+  end
+
+  describe '#category_options' do
+    it 'returns the i18n judgement categories hash' do
+      consumer = [t('activerecord.attributes.judgement.categories.consumer'), 'consumer']
+      social_security = [t('activerecord.attributes.judgement.categories.social_security'), 'social_security']
+      construction = [t('activerecord.attributes.judgement.categories.construction'), 'construction']
+      sfh = [t('activerecord.attributes.judgement.categories.sfh'), 'sfh']
+      sfi = [t('activerecord.attributes.judgement.categories.sfi'), 'sfi']
+
+      expect(helper.category_options).to eq [construction, sfh, sfi, social_security, consumer]
+    end
+  end
 end
